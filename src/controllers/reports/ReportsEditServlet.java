6ca46a2b4a83297dbcb3322d9445215d0f5f1838
hiroki.uematsu <uep74549@gmail.com>
2019-04-30 06:35:03 +0900

    Modyfy toppage

--- src/controllers/reports/ReportsEditServlet.java
+++ src/controllers/reports/ReportsEditServlet.java
@@ -42,7 +42,7 @@ public class ReportsEditServlet extends HttpServlet {
                 request.setAttribute("_token",request.getSession().getId());
                 request.getSession().setAttribute("report_id",r.getId());
         }
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/iews/reports/edit.jsp");
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/edit.jsp");
         rd.forward(request, response);
         }
     }
